eea8e33d38dd6199f6f65f88342780d8b6e0f06a
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-30 23:23:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,14 @@
-
+require 'pry'
 
 class Artist #what's the goal: compile  an Artists' songs, all of them
   attr_accessor :name
   @@song_count = 0
 
   def initialize(name)
+
     @name = name
     @songs = []
+
   end
 
   def name
@@ -16,30 +18,22 @@ class Artist #what's the goal: compile  an Artists' songs, all of them
   def songs
     @songs
   end
-
-
   def add_song(song)
-
-    self.songs<<song # add song into the array (self.songs vs @songs -- ask about this) self - adele lets adele know abt the song
+    @songs<<song # add song into the array (self.songs vs @songs -- ask about this) self - adele lets adele know abt the song
     song.artist = self #self referes to the artist I'M NOT SURE what the .artist is/comes from.
     @@song_count += 1
-
   end
-  #instead of calling add_song with a string, call it with an argument of a real song object  Song
 
-
-
-  def add_song_by_name(name)  # just a string comes in
+  def add_song_by_name(name)
     song = Song.new(name)
-    self.songs<<song
     song.artist = self
-    @@song_count +=1
-  end
+    @songs<<song
 
+    @@song_count += 1
+    #adele.songs.last.name
+  end
 
   def self.song_count
     @@song_count
   end
-
-
 end
--- lib/post.rb
+++ lib/post.rb
@@ -1,19 +1,15 @@
-require 'pry'
 class Post
-attr_accessor :title, :author, :name
-#how come author and name (from Author class are the same thing -- why am I so confused by thsi. )
-def initialize(title)
-  @title = title
+  attr_accessor :name, :author
 
-end
-
-def author_name
-  if self.author
-    self.author.name
-    binding.pry
-  else
-    nil
+  def initialize(name)
+    @name = name
   end
-end
 
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
+  end
 end
